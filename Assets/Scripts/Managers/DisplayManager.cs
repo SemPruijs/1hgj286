71cc8b06e881c840840753f3b4da06266323c6a9
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-10-17 22:19:39 +0200

    First commit

---
+++ Assets/Scripts/Managers/DisplayManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayManager : MonoBehaviour
+{
+    private static DisplayManager _instance;
+    public static DisplayManager Instance {
+        get {
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        } 
+    }
+    
+    public GameObject menu;
+    public GameObject inGame;
+    public GameObject playAgain;
+    public GameObject credit;
+    public GameObject settings;
+    
+    public void UpdateUi()
+    {
+        menu.SetActive(GameManager.Instance.state == GameManager.State.Menu);
+        inGame.SetActive(GameManager.Instance.state == GameManager.State.InGame);
+        playAgain.SetActive(GameManager.Instance.state == GameManager.State.PlayAgain);
+        credit.SetActive(GameManager.Instance.state == GameManager.State.Credit);
+        settings.SetActive(GameManager.Instance.state == GameManager.State.Settings);
+    }
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public State state;
+    private static GameManager _instance;
+         public static GameManager Instance {
+         get {
+             return _instance;
+         }
+    }
+    private void Awake()
+    {
+         if (_instance != null && _instance != this)
+         {
+            Destroy(this.gameObject);
+         } else {
+            _instance = this;
+         } 
+    }
+
+    public enum State
+    {
+        InGame,
+        Menu,
+        PlayAgain,
+        Credit,
+        Settings,
+        Pause
+    };
+
+    void Start()
+    {
+        Menu();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadCurrentScene();
+        }
+    }
+
+    public void Menu()
+    {
+        state = State.Menu;
+        DisplayManager.Instance.UpdateUi();
+    }
+
+    public void InGame()
+    {
+        state = State.InGame;
+        DisplayManager.Instance.UpdateUi();
+    }
+
+    public void Credit()
+    {
+        state = State.Credit;
+        DisplayManager.Instance.UpdateUi();
+    }
+
+    public void Settings()
+    {
+        state = State.Settings;
+        DisplayManager.Instance.UpdateUi();
+    }
+
+    public void PlayAgain()
+    {
+        state = State.PlayAgain;
+        DisplayManager.Instance.UpdateUi();
+    }
+
+    public void Pause()
+    {
+        state = State.Pause;
+        DisplayManager.Instance.UpdateUi();
+    }
+    
+    public void ReloadCurrentScene() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void RemoveAllObjectsWithTag(string tag)
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag (tag);
+
+        foreach (var g in gameObjects)
+        {
+            Destroy(g);
+        }
+    }
+}
